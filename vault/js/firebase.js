63bd33ec97d15ab28daf97f15980506337403d42
magaonlinegames <98446846+magaonlinegames@users.noreply.github.com>
2026-08-21 03:12:15 -0500

    Add files via upload

---
+++ vault/js/firebase.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var firebaseConfig = {
+        apiKey: "AIzaSyAv0EaIc5pc2mvvJKnB0bZ-Lot4zeu5kR8",
+        authDomain: "randy-53a65.firebaseapp.com",
+        projectId: "randy-53a65",
+        storageBucket: "randy-53a65.firebasestorage.app",
+        messagingSenderId: "924765598865",
+        appId: "1:924765598865:web:e27f42c6ee6cb554d07b30"
+    };
+
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+});
