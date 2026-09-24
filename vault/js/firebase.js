01214724140fb83073223b9967622153f9cbc7f7
magaonlinegames <98446846+magaonlinegames@users.noreply.github.com>
2026-09-24 06:28:43 -0700

    Update firebase.js

--- vault/js/firebase.js
+++ vault/js/firebase.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     var firebaseConfig = {
-        apiKey: "AIzaSyAv0EaIc5pc2mvvJKnB0bZ-Lot4zeu5kR8",
-        authDomain: "randy-53a65.firebaseapp.com",
-        projectId: "randy-53a65",
-        storageBucket: "randy-53a65.firebasestorage.app",
-        messagingSenderId: "924765598865",
-        appId: "1:924765598865:web:e27f42c6ee6cb554d07b30"
+        apiKey: "AIzaSyDs1iHCLo3aMYqTqkt_fMNmrvVY0o43UKU",
+        authDomain: "bankgame-9e3da.firebaseapp.com",
+        databaseURL: "https://bankgame-9e3da.firebaseio.com",
+        projectId: "bankgame-9e3da",
+        storageBucket: "bankgame-9e3da.appspot.com",
+        messagingSenderId: "550149232838",
+        appId: "1:550149232838:web:4b4564d2115703ea181fc7"
     };
 
 
